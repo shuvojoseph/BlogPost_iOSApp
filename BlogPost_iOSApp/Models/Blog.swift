4c4833c0f9153749440a8a8151a804293fd73e20
shuvojoseph <shuvo.ewu.cse@gmail.com>
2025-08-28 14:13:22 +0600

    #Updated BlogOwner Struct so that it can parse both "name" & "username" from the api response #Convert id to Sting so that it will also sync with DotNet GUID ID later. #Update Insert working fine now.

--- BlogPost_iOSApp/Models/Blog.swift
+++ BlogPost_iOSApp/Models/Blog.swift
@@ -14,10 +14,71 @@ struct Blog: Codable, Identifiable, Hashable {
     var owners: [BlogOwner]
     var lastUpdateTime: String?
 
-    struct BlogOwner: Codable, Hashable {
-        let id: String
-        let name: String
-        let email: String
+    struct BlogOwner: Codable, Identifiable, Hashable {
+        var id: String
+        var name: String
+        var email: String
+        
+        enum CodingKeys: String, CodingKey {
+            case id
+            case name
+            case username
+            case email
+        }
+        
+        init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            
+            // Handle Int or String id
+            if let intId = try? container.decode(Int.self, forKey: .id) {
+                self.id = String(intId)
+            } else {
+                self.id = try container.decode(String.self, forKey: .id)
+            }
+            
+            // Prefer "name", fallback to "username"
+            if let name = try? container.decode(String.self, forKey: .name) {
+                self.name = name
+            } else {
+                self.name = try container.decode(String.self, forKey: .username)
+            }
+            
+            self.email = try container.decode(String.self, forKey: .email)
+        }
+        
+        func encode(to encoder: Encoder) throws {
+                    var container = encoder.container(keyedBy: CodingKeys.self)
+
+                    // Try encoding id as Int if possible, else as String
+                    if let intId = Int(id) {
+                        try container.encode(intId, forKey: .id)
+                    } else {
+                        try container.encode(id, forKey: .id)
+                    }
+
+                    try container.encode(name, forKey: .name)
+                    try container.encode(email, forKey: .email)
+                }
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case id, title, details, owners, lastUpdateTime
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        // Handle Int or String id
+        if let intId = try? container.decode(Int.self, forKey: .id) {
+            self.id = String(intId)
+        } else {
+            self.id = try container.decode(String.self, forKey: .id)
+        }
+        
+        self.title = try container.decode(String.self, forKey: .title)
+        self.details = try container.decode(String.self, forKey: .details)
+        self.owners = try container.decode([BlogOwner].self, forKey: .owners)
+        self.lastUpdateTime = try? container.decode(String.self, forKey: .lastUpdateTime)
     }
 
     // convenience: owner emails
