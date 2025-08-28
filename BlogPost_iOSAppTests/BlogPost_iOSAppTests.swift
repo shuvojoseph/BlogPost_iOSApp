8c9996c66e67728c753139f106f74a8ab1c29f94
shuvojoseph <shuvo.ewu.cse@gmail.com>
2025-08-28 20:09:01 +0600

    #Write Unit test for name & username decoding.

--- BlogPost_iOSAppTests/BlogPost_iOSAppTests.swift
+++ BlogPost_iOSAppTests/BlogPost_iOSAppTests.swift
@@ -6,6 +6,8 @@
 //
 
 import Testing
+import Foundation
+@testable import BlogPost_iOSApp
 
 struct BlogPost_iOSAppTests {
 
@@ -13,4 +15,32 @@ struct BlogPost_iOSAppTests {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
     }
 
+    @Test func testUserDecodingWithUsername() async throws {
+        let jsonString = """
+            {
+                "id": 1,
+                "username": "shuvo",
+                "email": "shuvo@example.com"
+            }
+            """
+        let jsonData = jsonString.data(using: String.Encoding.utf8)!
+        
+        let user = try JSONDecoder().decode(User.self, from: jsonData)
+        #expect(user.username == "shuvo")
+        #expect(user.email == "shuvo@example.com")
+    }
+    
+    @Test func testUserDecodingWithNameInsteadOfUsername() async throws {
+        let json = """
+            {
+                "id": "2",
+                "name": "joseph",
+                "email": "joseph@example.com"
+            }
+            """.data(using: .utf8)!
+        
+        let user = try JSONDecoder().decode(User.self, from: json)
+        #expect(user.username == "joseph")
+        #expect(user.email == "joseph@example.com")
+    }
 }
